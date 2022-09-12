f1e75724e743ab6b47b713072058257575aeeee2
David Gallo <david.gallo747@gmail.com>
2022-09-12 11:58:37 -0400

    error handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 def play_alarm():
-            for i in range(0, 3):
+            for i in range(0, 30):
                 winsound.Beep(600, 1000)
                 winsound.Beep(800, 1000)
 
@@ -31,19 +31,19 @@ while True:
         # PAGE 3
         citizenship = driver.find_element(By.ID, 'xi-sel-400')
         citizenship.click()
-        time.sleep(2)
+        time.sleep(3)
         citizenship.send_keys("Lebanon")
         citizenship.send_keys(Keys.RETURN)
 
         num_applicants = driver.find_element(By.ID, 'xi-sel-422')
         num_applicants.click()
-        time.sleep(2)
+        time.sleep(3)
         num_applicants.send_keys("one person")
         num_applicants.send_keys(Keys.RETURN)
 
         family = driver.find_element(By.ID, 'xi-sel-427')
         family.click()
-        time.sleep(2)
+        time.sleep(3)
         family.send_keys("no")
         family.send_keys(Keys.RETURN)
 
@@ -56,21 +56,24 @@ while True:
         studying_permit = driver.find_element(By.CSS_SELECTOR, "#inner-451-0-1 > div > div:nth-child(2) > div > div:nth-child(4) > label")
         studying_permit.click()
 
-        time.sleep(5)
+        time.sleep(9)
         finish_button = driver.find_element(By.CSS_SELECTOR, '#applicationForm\:managedForm\:proceed')
         finish_button.click()
 
         # PAGE 4
-        time.sleep(30)
+        time.sleep(50)
         assert "Service selection" in driver.title
         if "There are currently no dates available for the selected service!" in driver.page_source:
-            driver.save_screenshot(f'logs_neg/{dt_string}.png')
+            print("No appointment found,", dt_string)
+            driver.save_screenshot(f'./logs/neg/{dt_string}.png')
             driver.close()
         else:
-            driver.save_screenshot(f'logs_pos/{dt_string}.png')
+            print("Found appointment,", dt_string)
+            driver.save_screenshot(f'./logs/pos/{dt_string}.png')
             play_alarm()
             while True:
                 time.sleep(10000)
-    except:
-        driver.save_screenshot(f'logs_err/{dt_string}.png')
+    except Exception as e:
+        print(f"{dt_string}. Error: {e}")
+        driver.save_screenshot(f'./logs/err/{dt_string}.png')
         driver.close()
